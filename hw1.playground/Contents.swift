82fa9dbd2dd50daeea9ef19d15beb8ef7bcc5372
ddianalim <dianalim1117@gmail.com>
2024-09-18 10:03:37 -0400

    setting up structs, enums, protocol

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -2,17 +2,46 @@ import SwiftUI
 
 // TODO: Declare any additional structs, classes, enums, or protocols here!
 
+struct Location {
+    let name: String
+    let description: String
+    var exits: [String: String]
+    var items: [Item]
+}
+
+struct Item {
+    let name: String
+    let description: String
+    var isPickedUp: Bool = false
+}
+
+enum GameState {
+    case atBasecamp
+    case onMountain
+    case atSummit
+    case descending
+    case gameOver
+}
+
+protocol GameObject {
+    var name: String { get }
+    var description: String { get }
+}
+
+extension Location: GameObject {}
+extension Item: GameObject {}
+
 /// Declare your game's behavior and state in this struct.
 ///
 /// This struct will be re-created when the game resets. All game state should
 /// be stored in this struct.
-struct YourGame: AdventureGame {
+struct EverestGame: AdventureGame {
     /// Returns a title to be displayed at the top of the game.
     ///
     /// You can generate this dynamically based on your game's state.
     var title: String {
         // TODO: Change this title
-        return "Generic Adventure Game"
+        return "Climbing Mt. Everest 🏔️"
     }
     
     /// Runs at the start of every game.
@@ -21,9 +50,11 @@ struct YourGame: AdventureGame {
     ///
     /// - Parameter context: The object you use to write output and end the game.
     mutating func start(context: AdventureGameContext) {
-        // TODO: Remove this and implement logic to start your game!
-        playIntroduction()
-        context.write("Welcome to Generic Adventure Game!")
+        //        playIntroduction()
+        context.write("Welcome to the Mt. Everest Climbing Adventure!")
+        context.write("You are an experienced climber attempting to summit Mt. Everest.")
+        context.write("Your journey begins at Basecamp. Good luck, and be careful!")
+        // TODO: write something about acclimitization
     }
     
     /// Runs when the user enters a line of input.
@@ -52,9 +83,11 @@ struct YourGame: AdventureGame {
     mutating func handle(input: String, context: AdventureGameContext) {
         // TODO: Parse the input and implement your game logic!
         context.write("You decide to \(input). It's not very effective.")
+        // switch statement here
+        // context.write("You have been eaten by a grue.")
     }
 }
 
 // Leave this line in - this line sets up the UI you see on the right.
 // Update this if you rename your AdventureGame implementation.
-YourGame.display()
+EverestGame.display()
